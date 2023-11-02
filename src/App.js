0541c7d60ae53443430f01ad92c2147f3148a48a
Shivendra <shivendra02shah@gmail.com>
2023-11-02 12:34:21 +0530

    my First Commit q! a!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import HomePage from "./components/Home/HomePage";
+import { HTMLSideBar } from "./components/SideBar/HTMLSideBar";
+import HTMLIntroduction from "./components/Pages/HTML/Introduction";
+import HTMLBasic from "./components/Pages/HTML/Basic";
+import HTMLElements from "./components/Pages/HTML/Elements";
+import { CSSSideBar } from "./components/SideBar/CSSSideBar";
+import { JSSideBar } from "./components/SideBar/JSSideBar";
+import { ReactSideBar } from "./components/SideBar/ReactSideBar";
+import CSSIntroduction from "./components/Pages/CSS/Introduction";
+import { SignUp } from "./components/SignUp/SignUp";
+import { LogIn } from "./components/LogIn/LogIn";
+import JavaScriptIntroduction from "./components/Pages/JavaScript/Introduction";
+import ReactIntroduction from "./components/Pages/React/Introduction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        {/* HTML */}
+        <Route path="/html" element={<HTMLSideBar />}>
+          <Route index element={<HTMLIntroduction />} />
+          <Route
+            path="/html/html_introduction"
+            element={<HTMLIntroduction />}
+          />
+          <Route path="/html/html_basic" element={<HTMLBasic />} />
+          <Route path="/html/html_elements" element={<HTMLElements />} />
+        </Route>
+
+        {/* CSS */}
+        <Route path="/css" element={<CSSSideBar />}>
+          <Route index element={<CSSIntroduction />} />
+          <Route path="/css/css_introduction" element={<CSSIntroduction />} />
+        
+        </Route>
+
+        {/* JavaScript */}
+        <Route path="/javascript" element={<JSSideBar />}>
+          <Route index element={<JavaScriptIntroduction />} />
+          <Route
+            path="/javascript/js_intro"
+            element={<JavaScriptIntroduction />}
+          />
+         
+        </Route>
+
+        {/* React */}
+        <Route path="/react" element={<ReactSideBar />}>
+          <Route index element={<ReactIntroduction />} />
+          <Route path="/react/react_intro" element={<ReactIntroduction />} />
+        
+        </Route>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
